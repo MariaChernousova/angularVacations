63c286862fe4dccdefa269664d3d1b3111e39399
MariaChernousova <chernousova.maria.kr@gmail.com>
2021-03-06 15:22:27 +0200

    add structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ApiComponent } from './api/api.component';
+import { UtilsComponent } from './utils/utils.component';
+import { ModalComponent } from './modal/modal.component';
+import { TableComponent } from './table/table.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiComponent,
+    UtilsComponent,
+    ModalComponent,
+    TableComponent,
+    NavigationComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
